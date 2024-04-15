8c417d0213603144b60ee3bd79f21dd93d6afee9
kyj9447 <kyj9447@gmail.com>
2024-04-15 21:01:48 +0900

    validation_result 작성시 중복문자열 제거하도록 수정

--- main.py
+++ main.py
@@ -22,7 +22,22 @@ elif choice == "2":
 
     image = "signed_original.png"
 
+    # validate 결과 (String)
     validation = validateImage(image)
+
+    # 줄바꿈 기준으로 split한 배열
+    validationarr = validation.split("\n")
+
+    # 중복 제거
+    deduplicated = []
+    for i in range(len(validationarr)):
+        if i == 0 or validationarr[i] != validationarr[i-1]:
+            deduplicated.append(validationarr[i])
+
+    # 중복 제거된 결과
+    validation = "\n".join(deduplicated)
+
+    # 파일로 저장
     with open("validation_result.txt", "w") as file:
         file.write(validation)
     print("validation_result.txt 파일이 작성되었습니다.")
